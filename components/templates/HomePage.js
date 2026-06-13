2c5d3434670022af19d6e4b82952c590b928afd3
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2026-06-13 15:10:10 +0330

    Create a home page

---
+++ components/templates/HomePage.js
@@ -0,0 +1,23 @@
+import Attributes from "../modules/Attributes"
+import Banner from "../modules/Banner"
+import Companies from "../modules/Companies"
+import Definition from "../modules/Definition"
+import Instruction from "../modules/Instruction"
+import Guide from "../modules/Guide"
+import styles from "./HomePage.module.css"
+import Restriction from "../modules/Restriction"
+function HomePage() {
+    return (
+        <div className={styles.container}>
+            <Banner />
+            <Attributes />
+            <Definition />
+            <Companies />
+            <Instruction />
+            <Guide />
+            <Restriction />
+        </div>
+    )
+}
+
+export default HomePage
